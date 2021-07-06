1d28d986c7bedf8de0a0f571a4f467e0c4df86d5
erica-joseph <ericagjoseph@outlook.com>
2021-07-05 23:58:55 -0400

    Tests

---
+++ src/test/java/ucf/assignment/methods/saveExternallyTest.java
@@ -0,0 +1,10 @@
+package ucf.assignment.methods;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class saveExternallyTest {
+
+    //@Test
+    //After input of new file name, will attempt to read file. If file is found, assert true.
+
+}
